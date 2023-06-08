ed3b14ecfcc1c88c26b375c203109e77261619e3
vishnutak07 <121621394+vishnutak07@users.noreply.github.com>
2023-06-08 09:27:23 +0530

    initial commit\

---
+++ precache-manifest.a04e8399432d9c13af5de15044135880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "53f22fba555a8eabd183",
+    "url": "/static/css/main.31619f10.chunk.css"
+  },
+  {
+    "revision": "53f22fba555a8eabd183",
+    "url": "/static/js/main.53f22fba.chunk.js"
+  },
+  {
+    "revision": "6e5853b3ee902d8ff7cf",
+    "url": "/static/js/1.6e5853b3.chunk.js"
+  },
+  {
+    "revision": "80539924313a125e840a",
+    "url": "/static/js/2.80539924.chunk.js"
+  },
+  {
+    "revision": "cc2112b961d29481f6c9",
+    "url": "/static/js/runtime~main.cc2112b9.js"
+  },
+  {
+    "revision": "f15d7e3166547d3f285fd4f95ef2dcc6",
+    "url": "/static/media/1484.f15d7e31.gif"
+  },
+  {
+    "revision": "4aaf00f24e39441813b1db2685ef8bf8",
+    "url": "/index.html"
+  }
+];
